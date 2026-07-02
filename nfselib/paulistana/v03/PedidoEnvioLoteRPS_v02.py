7bf7a3ff1cc2d1c645b04e59150b86a082b1c7ce
kaynnan <kaynnanx1@gmail.com>
2026-07-02 14:33:41 -0300

    [TESTE

--- nfselib/paulistana/v03/PedidoEnvioLoteRPS_v02.py
+++ nfselib/paulistana/v03/PedidoEnvioLoteRPS_v02.py
@@ -112,7 +112,7 @@ def parsexmlstring_(instring, parser=None, **kwargs):
 #
 
 try:
-    from generatedsnamespaces import GenerateDSNamespaceDefs as GenerateDSNamespaceDefs_
+    from .generatedsnamespaces import GenerateDSNamespaceDefs as GenerateDSNamespaceDefs_
 except ModulenotfoundExp_ :
     GenerateDSNamespaceDefs_ = {}
 try:
